edc96d1b6c214535d37446c7bf9122e25c081373
Praise Magoche <praisemagoche@gmail.com>
2025-07-30 14:18:23 +0200

    Add files via upload

---
+++ social-links.js
@@ -0,0 +1,11 @@
+const socialBtn = document.querySelector('.js-button');
+const socialCont = document.getElementById('container');
+//socialCont.style.display = "none"
+
+socialBtn.addEventListener('click', () => {
+    if (socialCont.style.display === "none") {
+        socialCont.style.display = "grid"
+    } else {
+        socialCont.style.display = "none"
+    }
+})
