35563be4995e2d958d0e47b7333870a60fe97571
Praise Magoche <praisemagoche@gmail.com>
2025-07-30 14:41:42 +0200

    Update social-links.js

--- social-links.js
+++ social-links.js
@@ -1,6 +1,6 @@
 const socialBtn = document.querySelector('.js-button');
 const socialCont = document.getElementById('container');
-//socialCont.style.display = "none"
+socialCont.style.display = "none"
 
 socialBtn.addEventListener('click', () => {
     if (socialCont.style.display === "none") {
